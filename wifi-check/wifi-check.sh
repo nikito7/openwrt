a9f76a88fe23aee3ec2ad945ad0af2c27b0ab00e
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-10-21 19:12:32 +0100

    Update wifi-check.sh

--- wifi-check/wifi-check.sh
+++ wifi-check/wifi-check.sh
@@ -2,58 +2,43 @@
 
 ### wifi-check.sh ###
 
-dev=$1
+ip1=$1
+ip2=$2
 
 if [ ! "$1" ]
 then
-echo usage: $0 wlan0
-echo usage: $0 lo // test
-echo usage: $0 // show all
+echo usage: $0 ip1 ip2
 fi
 
-function getbytes()
-{
-ifconfig $dev | grep "TX packets" | awk '{ print $2}' | awk -F : '{ print $2 }'
-}
-
-###
-
-A=$(getbytes)
-echo debug: A $A
-ping -c 2 -I br-lan ff02::1 > /dev/null
-sleep 10
-B=$(getbytes)
-echo debug: B $B
-
-if [ "$A" == "$B" ]
+if [ ! "$2" ]
 then
-
-echo debug: wifi cycle
-wifi
-sleep 10
-
-C=$(getbytes)
-echo debug: if fail will reboot.
-echo debug: Press Ctrl+C now to abort.
-echo debug: C $C
-ping -c 2 -I br-lan ff02::1 > /dev/null
-sleep 10
-D=$(getbytes)
-echo debug: D $D
-
-if [ "$C" == "$D" ]
-then
-echo debug: reboot now
-reboot
+echo usage: $0 ip1 ip2
 fi
 
-else
-echo debug: OK
-fi
+status=0
+ping -c 2 $ip1 > /dev/null
+status=$?
+ping -c 2 $ip2 > /dev/null
+status=$(($status + $?))
+
+echo debug: status $status
+
+#echo debug: wifi cycle
+#wifi
+#sleep 10
+
+
+#sleep 10
+
+#echo debug: reboot now
+#reboot
+
+
+
 
 ### loop ###
 
-sleep 30; /bin/sh $0 $dev &
+#sleep 30; /bin/sh $0 $dev &
 
 ### wifi-check.sh ###
 ##
