7daaf31d5db57fde1a98111f03b5107251c13f10
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-10-22 18:07:52 +0100

    Update wifi-check.sh

--- wifi-check/wifi-check.sh
+++ wifi-check/wifi-check.sh
@@ -1,59 +1,46 @@
 #!/bin/sh
 
 ### wifi-check.sh ###
-
-if [ ! "$1" ] || [ ! "$2" ]
-then
-echo usage: $0 ip1 ip2
-ip1=10.1.0.1
-ip2=10.1.0.2
-else
-ip1=$1
-ip2=$2
-fi
-
-### ### ###
-
-echo debug: step 1
+#
+# using ipv6 multicast
+#
 
 status=0
-ping -c 2 $ip1 > /dev/null
-status=$?
-ping -c 2 $ip2 > /dev/null
-status=$(($status + $?))
+status=$(ping -I br-lan ff02::1 -c 2 | grep DUP | wc -l)
 
 echo debug: status $status
 
-if [ $status -gt 1 ]
+### if begin ###
+
+if [ $status -lt 2 ]
 then
+
+#
+
 echo debug: wifi cycle
 wifi
-fi
 
 sleep 10
 
-### ### ###
-
-echo debug: step 2
+#
 
 status=0
-ping -c 2 $ip1 > /dev/null
-status=$?
-ping -c 2 $ip2 > /dev/null
-status=$(($status + $?))
+status=$(ping -I br-lan ff02::1 -c 2 | grep DUP | wc -l)
 
-echo debug: status $status
-
-if [ $status -gt 1 ]
+if [ $status -lt 2 ]
 then
 echo debug: reboot now
 sleep 1
 reboot
 fi
 
+### end if ###
+
+fi
+
 ### loop ###
 
-sleep 20 && /bin/sh $0 $1 $2 &
+sleep 20 && /bin/sh $0 &
 
 ### wifi-check.sh ###
 ##
