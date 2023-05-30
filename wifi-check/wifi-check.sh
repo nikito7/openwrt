3e86839349000c8d24c82757244776964ef563af
nikito7 <45373783+nikito7@users.noreply.github.com>
2023-05-31 00:14:31 +0100

    Update wifi-check.sh

--- wifi-check/wifi-check.sh
+++ wifi-check/wifi-check.sh
@@ -2,17 +2,23 @@
 
 ### wifi-check.sh ###
 
-iplist="10.1.0.1 10.1.0.5"
-trigger=1
+iplist="10.1.0.1"
+trigger=5
 
 ###
 
+if [ $1 ]
+then
+status=$1
+else
 status=0
+fi
+
 excode=0
 
 for ip in $iplist
 do
-sleep 10
+sleep 5
 ping -c 2 -W 1 $ip > /dev/null
 excode=$?
 status=$(($status + $excode))
